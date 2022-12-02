9429f4436fdc74bd2297fedb53fa7f069e95ee10
SimoVisc <simonaviscardivs@gmail.com>
2022-12-02 11:38:47 +0100

    installa flag-icons e aggiunge le bandiere per indicare la lingua originale

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -2,13 +2,27 @@
     export default {
     name: "AppCard",
     props:{
-        info: Object
+      info: Object
+    },
+    methods: {
+        getFlag(lang) {
+            if (lang === "en") {
+                return "gb";
+            } else if (lang === "ko") {
+                return "kr";
+            } else return lang;
+        },
     }
    }
 </script>
 
 <template>
-    <h3>{{ info.title}}</h3>
+    <ul>
+        <li>{{ info.title}}</li>
+        <li>{{ info.original_title}}</li>
+        <li><span :class="`fi fi-${getFlag(info.original_language)}`"></span></li>
+        <li>{{ info.vote_average}}</li>
+    </ul>
 </template>
 
 <style lang="scss">
